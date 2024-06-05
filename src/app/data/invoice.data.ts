6b421660c5ac66e47d79e1dd305979a2f100f0fc
Eleazar Alfriadez <ealfriadez@gmail.com>
2024-06-05 12:08:18 -0500

    Plantilla realizada de facturas

---
+++ src/app/data/invoice.data.ts
@@ -0,0 +1,40 @@
+import { Invoice } from "../models/invoice";
+
+export const invoiceData: Invoice = {
+    id: 1,
+    name: 'Componentes de PC',
+    client: {
+        name: 'Eleazar',
+        lastName: 'Alfriadez',
+        address:{
+            country: 'Peru',
+            city: 'Lima',
+            street: 'Calle uno',
+            number: 20
+        }
+    },
+    company: {
+        name: 'UNFV',
+        fiscalNumber: 3123123
+    },
+    items: [
+        {
+             id: 1,
+             product: 'CPU con case de alumnio',
+             price: 800,
+             quantity: 5    
+        },
+        {
+            id: 1,
+            product: 'Teclado mecanico TRONSMART',
+            price: 100,
+            quantity: 7
+        },
+        {
+            id: 1,
+            product: 'Monitor SAMSUNG',
+            price: 1500,
+            quantity: 15
+        }
+    ]
+}
