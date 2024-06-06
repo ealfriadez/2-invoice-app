3c36787cdd378b0346ed07b07f1dd96c98fc5e21
Club Fitnes 2020 <clubfitnes2020@gmail.com>
2024-06-05 23:33:48 -0500

    Finalizando el dia

--- src/app/components/invoice/invoice.component.ts
+++ src/app/components/invoice/invoice.component.ts
@@ -5,12 +5,18 @@ import { ClientViewComponent } from '../client-view/client-view.component';
 import { CompanyViewComponent } from '../company-view/company-view.component';
 import { InvoiceViewComponent } from '../invoice-view/invoice-view.component';
 import { ListItemsComponent } from '../list-items/list-items.component';
-import { RowItemComponent } from '../row-item/row-item.component';
+import { TotalComponent } from '../total/total.component';
 
 @Component({
   selector: 'app-invoice',
   standalone: true,
-  imports: [ClientViewComponent, CompanyViewComponent, InvoiceViewComponent, ListItemsComponent, RowItemComponent],
+  imports: [
+              ClientViewComponent, 
+              CompanyViewComponent, 
+              InvoiceViewComponent, 
+              ListItemsComponent,
+              TotalComponent
+            ],
   templateUrl: './invoice.component.html'
 })
 export class InvoiceComponent implements OnInit{  
--- src/app/services/invoice.service.ts
+++ src/app/services/invoice.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Invoice } from '../models/invoice';
 import { invoiceData } from '../data/invoice.data';
+import { Item } from '../models/items';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,18 @@ export class InvoiceService {
   constructor() { }
 
   getInvoice(): Invoice{
-    return this.invoice;
+    const total = this.calculateTotal();
+    return {...this.invoice, total};
+  }
+
+  calculateTotal(){
+    //  let total = 0;
+
+    //  this.invoice.items.forEach(item => {
+    //    total += item.total();
+    //  });
+
+    //  return total;
+    return this.invoice.items.reduce((acumulator, item) => acumulator + (item.price * item.quantity), 0);
   }
 }
